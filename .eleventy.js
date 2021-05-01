505e3858842dd266be79fa57578297e5d00311f4
Tim <tculverhouse@gmail.com>
2021-05-01 02:29:57 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,16 @@
 const md5 = require('md5');
 
+// Passthrough files
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("assets/img");
+    eleventyConfig.addPassthroughCopy("style.css");
+};
 
 // Define hash filter, returns md5 hash of input
 module.exports = function (eleventyConfig) {
-
     eleventyConfig.addFilter("hash", function (string) {
         var result = md5(string);
         return md5(string);
     });
 };
 
-// Passthrough files
-module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets/img");
-    eleventyConfig.addPassthroughCopy("style.css");
-};
